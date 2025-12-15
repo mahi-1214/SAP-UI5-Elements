034637fd310d93e08713f3fe8573dbef9fae7da0
Avula Mahipal Reddy <y22cse010@chalapathiengg.ac.in>
2025-12-15 05:52:14 +0000

    initial commit

---
+++ project2/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.mahipal.project2.project2.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ project2/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/mahipal/project2/project2/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
